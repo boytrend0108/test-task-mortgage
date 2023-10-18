5632456008735bd202a7bd14c8d19329f1ab4785
boytrend0108 <boytrend@gmail.com>
2023-10-18 23:48:45 +0300

    change background path

--- src/store/modules/variables.js
+++ src/store/modules/variables.js
@@ -4,15 +4,15 @@ const variables = {
       { title: 'Ипотека от 4,3%', 
         description: 'Выгодные условия при оформлении у банков-партнеров',
         bg: 'src/assets/images/offers/offer-bg-1.png',
-        bg_1: 'src/assets/images/offers/shadow-bg.png'
+        bg_1: './src/assets/images/offers/shadow-bg.png'
       },
       { title: 'Рассрочка 0%',
         description: 'Гибкие условия по размеру первоначального взноса, платежам и срокам',
-        bg: 'src/assets/images/offers/offer-bg-2.png'
+        bg: './src/assets/images/offers/offer-bg-2.png'
       },
       { title: 'Покупка в Trade-in', 
         description: 'Выгодный обмен старой квартиры на новую квартиру или апартаменты',
-        bg: 'src/assets/images/offers/offer-bg-3.png'
+        bg: './src/assets/images/offers/offer-bg-3.png'
       },
     ]
   }),
