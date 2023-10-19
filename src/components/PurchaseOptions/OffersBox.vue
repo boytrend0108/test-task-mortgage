45f53ba71f07df462ae7848f82f43574a029c2b9
boytrend0108 <boytrend@gmail.com>
2023-10-20 00:14:01 +0300

    fix bnt position

--- src/components/PurchaseOptions/OffersBox.vue
+++ src/components/PurchaseOptions/OffersBox.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="offers">
      <div class="offer" v-for="(item, i) in items" :key="i">
-        <img src="../../assets/images/offers/offer-mask.png" class="offer__mask">
-        <img src='../../assets/images/offers/shadow-bg.png' class="offer__shadow">
-
-        <div 
-          class="offer__content"
-          :style="{ backgroundImage: `url(${item.bg})`}"
-        ></div>
+        <img src="@/assets/images/offers/offer-mask.png" class="offer__mask">
+        <img src='@/assets/images/offers/shadow-bg.png' class="offer__shadow">
+        <img :src="`/test-task-mortgage/${item.bg}`"  class="offer__content">
 
         <p class="offer__title">{{ item.title }}</p>
 
@@ -34,24 +30,24 @@
 </script>
 
 <style scoped lang="scss">
-  .offers {
-    display: flex;
-    gap: 36px;
-    align-items: center;
-    justify-content: center;
-  }
+.offers {
+  display: flex;
+  gap: 36px;
+  align-items: center;
+  justify-content: center;
+}
 
-  .offer {
-    position: relative;
-    width: 420px;
-    height: 302px;
-    overflow: hidden;
-    cursor: pointer;
+.offer {
+  position: relative;
+  width: 420px;
+  height: 302px;
+  overflow: hidden;
+  cursor: pointer;
 
-    &:hover {
+  &:hover {
 
-      .offer {
-        &__content{
+    .offer {
+      &__content {
         transform: scale(1.1);
       }
 
@@ -64,78 +60,76 @@
         transform: scale(1.1) rotate(90deg);
       }
 
-      }
     }
+  }
 
-    &__mask {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      z-index: 1;
-      background-image: url(../../assets/images/offers/offer-mask.png);
-    }
+  &__mask {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: 1;
+  }
 
-    &__shadow {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      z-index: 2;
-    }
+  &__shadow {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+  }
 
- 
-    &__content {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      transition: transform ease-in $transition-duration;
-    }
-     
-      &__btn {
-        position: absolute;
-        bottom: 9px;
-        right: 9px;
-        z-index: 2;
-        transform: scale(1);
+  &__content {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    transition: transform ease-in $transition-duration;
+  }
 
-        transition: all ease-in $transition-duration;
-      }
+  &__btn {
+    position: absolute;
+    bottom: 4px;
+    right: 4px;
+    z-index: 2;
+    transform: scale(1);
 
-      &__title {
-        display: flex;
-        padding: 10px 12px;
-        box-sizing: border-box;
-        align-items: center; 
-        position: absolute;
-        top: 30px;
-        left: 30px;
-        height: 54px;
-        background-color: $color-bg;
-        border-radius: 10px;
+    transition: all ease-in $transition-duration;
+  }
 
-        color: $color-title-secondary;
-        font-size: 26px;
-        font-weight: 500;
-        line-height: 130%; 
+  &__title {
+    display: flex;
+    padding: 10px 12px;
+    box-sizing: border-box;
+    align-items: center;
+    position: absolute;
+    top: 30px;
+    left: 30px;
+    height: 54px;
+    background-color: $color-bg;
+    border-radius: 10px;
 
-        transition: all ease-in $transition-duration;
-      }
+    color: $color-title-secondary;
+    font-size: 26px;
+    font-weight: 500;
+    line-height: 130%;
 
-      &__description {
-        color: $color-bg;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 140%;
+    transition: all ease-in $transition-duration;
+  }
 
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 18px 21px 0 0;
-        border-top: 1px solid $color-bg;
-        max-width: 295px;
+  &__description {
+    color: $color-bg;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 140%;
 
-        position: absolute;
-        bottom: 44px;
-        left: 20px;
-      }
-    }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 18px 21px 0 0;
+    border-top: 1px solid $color-bg;
+    max-width: 295px;
+
+    position: absolute;
+    bottom: 44px;
+    left: 20px;
+  }
+}
 </style>
